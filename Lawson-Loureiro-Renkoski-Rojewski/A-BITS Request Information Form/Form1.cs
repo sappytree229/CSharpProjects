46c8cf8015635d48cdf3d8c8b0ed20698fd746be
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-11-14 13:40:34 -0600

    Last Push of the day, Database connected

--- Lawson-Loureiro-Renkoski-Rojewski/A-BITS Request Information Form/Form1.cs
+++ Lawson-Loureiro-Renkoski-Rojewski/A-BITS Request Information Form/Form1.cs
@@ -14,18 +14,20 @@ namespace A_BITS_Request_Information_Form
         private void SubmitButton_Click(object sender, EventArgs e)
         {
             //Initiate SQL Connection variables
-            String myConnectionString = "";
-            String mySQLInsert = "";
-            //OleDbConnection myConnection;
-            //OleDbCommand myCommand;
+            String abitsDatabaseString = "";
+            String abitsInsertCommandString = "";
+            OleDbConnection abitsConnection;
+            OleDbCommand abitsCommand;
 
-            myConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\sappy\\source\\repos\\sappytree229\\SystemsAnalysisAndDesign\\Lawson-Loureiro-Renkoski-Rojewski\\A-BITS Request Information Form\\ABITS.accdb\"";
-            mySQLInsert = "Insert into PROSPECTIVE_MEMBER_INFO (fName) values (" + FirstNameBox.Text + ")";
+            abitsDatabaseString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\sappy\\source\\repos\\sappytree229\\SystemsAnalysisAndDesign\\Lawson-Loureiro-Renkoski-Rojewski\\A-BITS Request Information Form\\ABITS.accdb\"";
+
+            // WORK IN PROGRESS!!!! abitsInsertCommandString = "Insert into PROSPECTIVE_MEMBER_INFO (fName) values (" + FirstNameBox.Text + ")";
+            
             //Initiate SQL Connection
-            //myConnection = new OleDbConnection(myConnectionString);
-            //myConnection.Open();
+            abitsConnection = new OleDbConnection(abitsDatabaseString);
+            abitsConnection.Open();
 
-            //myCommand = new OleDbCommand(mySQLInsert, myConnection);
+            abitsCommand = new OleDbCommand(abitsInsertCommandString);
 
             //Prospective Member variables
             string  prospectiveMemberFirstNameString = "";
@@ -35,8 +37,8 @@ namespace A_BITS_Request_Information_Form
 
             //Store User input in Prospective Member Variables
             prospectiveMemberFirstNameString = FirstNameBox.Text;
-            prospectiveMemberLastNameString = LastNameBox.Text;
-            prospectiveMemberEmailString = EmailBox.Text;
+            prospectiveMemberLastNameString  = LastNameBox.Text;
+            prospectiveMemberEmailString     = EmailBox.Text;
 
             //Change Sponsor boolean from false to true
             if (willProspectiveMemberSponsorAbitsBoolean = SponsorBox.Checked)
